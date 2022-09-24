0cc5df2545eca0a3b062ae372bf335d414265cfd
Mamman Abdulbasit <basttyy@gmail.com>
2022-09-24 09:54:33 +0100

    added get() query feature

--- example/testdb.php
+++ example/testdb.php
@@ -6,6 +6,7 @@ require './src/QueryBuilderWrapper.php';
 
 use Basttyy\ReactphpOrm\QueryBuilderWrapper;
 use Basttyy\ReactphpOrm\QueryBuilder;
+use Illuminate\Support\Collection;
 use React\MySQL\Factory;
 use React\MySQL\QueryResult;
 use React\Promise\PromiseInterface;
@@ -42,18 +43,11 @@ function runQuery(PromiseInterface|QueryBuilder $connection)
 
 function runGet(PromiseInterface|QueryBuilder $connection)
 {
-    $data = $connection->from('users')->where('status', 'inactive')->get();
-
-    print_r($data);
-    //$connection->from('users')->where('status', 'inactive')->query()->then(
-    //     function (QueryResult $command) {
-    //         print_r($command->resultRows);
-    //         echo count($command->resultRows) . ' row(s) in set' . PHP_EOL;
-    //     },
-    //     function (Exception $error) {
-    //         echo 'Error: ' . $error->getMessage() . PHP_EOL;
-    //     }
-    // );
+    $connection->from('users')->where('status', 'inactive')->get()
+        ->then(function(Collection $data) {
+            print_r($data->all());
+            echo $data->count() . ' row(s) in set' . PHP_EOL;
+    });
 }
 
 $connection->quit();
